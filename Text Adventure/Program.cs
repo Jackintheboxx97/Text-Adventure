592a9497a13aa652202856abc7844cbef1e87b9e
Jack in the Boxx <kdijahsweets@gmail.com>
2020-03-06 20:10:47 -0600

    New Game! Mirai

---
+++ Text Adventure/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Text_Adventure
+{
+   public static class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Mirai");
+            Console.WriteLine("You've been sent to the future. You are now in the year 3005.");
+            Console.ReadKey();
+        }
+    }
+}
