59bdbb039bd8f8df7e433807e345717e594fb0a3
Jack in the Boxx <kdijahsweets@gmail.com>
2020-03-12 04:14:42 -0500

    Changing the title and game

--- Text Adventure/Program.cs
+++ Text Adventure/Program.cs
@@ -1,31 +1,36 @@
 ﻿using System;
-
+using System.Collections.Generic;
 namespace Text_Adventure
 {
-   public static class Program
+    public static class Program
     {
         static void Main(string[] args)
         {
             Console.ForegroundColor = ConsoleColor.Blue;
 
             Console.Write(@"
-__________                       ________
-\____    /____   ____   ____    /  _____/
-  /     //  _ \ /    \_/ __ \  /   __  \ 
- /     /(  <_> )   |  \  ___/  \  |__\  \
-/_______ \____/|___|  /\___  >  \_____  /
-        \/          \/     \/         \/ 
-");
+___________.__             ________                          __      _____              __________                __          
+\__    ___/|  |__   ____   \_____  \  __ __   ____   _______/  |_  _/ ____\___________  \______   \_____    _____/  |_  ______
+  |    |   |  |  \_/ __ \   /  / \  \|  |  \_/ __ \ /  ___/\   __\ \   __\/  _ \_  __ \  |     ___/\__  \  /    \   __\/  ___/
+  |    |   |   Y  \  ___/  /   \_/.  \  |  /\  ___/ \___ \  |  |    |  | (  <_> )  | \/  |    |     / __ \|   |  \  |  \___ \ 
+  |____|   |___|  /\___  > \_____\ \_/____/  \___  >____  > |__|    |__|  \____/|__|     |____|    (____  /___|  /__| /____  >
+                \/     \/         \__>           \/     \/                                              \/     \/          \/ 
+
+
+       ");
 
             Console.ForegroundColor = ConsoleColor.DarkMagenta;
-            DelayWrite("Hey! Are you ok?");
-            Console.ReadLine();
-            DelayWrite("You don't look like you're from around here. Are you lost?");
-            Console.ReadLine();
+            DelayWrite("It is 7:00 am. You have a very important interview at " +
+                "10:00 am for a job that you've been trying to get for three year!");
+            DelayWrite("You havent gotten much sleep all night due to high levels of excitement. " +
+                "Would you like to keep sleeping?");
+            string userSays = Console.ReadLine();
+            if(userSays == "")
+            {
 
-            
+            }
         }
-        static void DelayWrite(string text, int charDelay = 75, bool DelayNewLine = true)
+        static void DelayWrite(string text, int charDelay = 100, bool DelayNewLine = true)
         {
             foreach(char c in text)
             {
