58a50febd0cd53393fd26d8ad91cb6c8514acc18
Jack in the Boxx <kdijahsweets@gmail.com>
2020-03-13 13:21:09 -0500

    Add Beep noise and more text

--- Text Adventure/Program.cs
+++ Text Adventure/Program.cs
@@ -1,9 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Threading;
+using System.Timers;
+
 namespace Text_Adventure
 {
     public static class Program
     {
+        
         static void Main(string[] args)
         {
             Console.ForegroundColor = ConsoleColor.Blue;
@@ -16,20 +20,34 @@ ___________.__             ________                          __      _____      
   |____|   |___|  /\___  > \_____\ \_/____/  \___  >____  > |__|    |__|  \____/|__|     |____|    (____  /___|  /__| /____  >
                 \/     \/         \__>           \/     \/                                              \/     \/          \/ 
 
-
        ");
 
+       
             Console.ForegroundColor = ConsoleColor.DarkMagenta;
-            DelayWrite("It is 7:00 am. You have a very important interview at " +
+            DelayWrite("It is 8:00 am. You have a very important interview at " +
                 "10:00 am for a job that you've been trying to get for three years!");
             DelayWrite("You haven't gotten much sleep all night due to high levels of excitement. " +
                 "Would you like to keep sleeping? yes/no");
             string userSays = Console.ReadLine().ToLower();
+
             if(userSays == "yes")
+            {
+                DelayWrite("I must warn you, going back to sleep now highly increases your" +
+                    " chances of oversleeping and missing your interview." +
+                    "Are you sure you want to keep sleeping? yes/no");
+            }            
+           string sleepResponse = Console.ReadLine().ToLower();
+            if(sleepResponse == "yes")
+            {
+                Console.WriteLine("You've overslept!");
+                Console.Beep();
+            }
+            else if(userSays == "no")
             {
 
             }
         }
+
         static void DelayWrite(string text, int charDelay = 100, bool DelayNewLine = true)
         {
             foreach(char c in text)
