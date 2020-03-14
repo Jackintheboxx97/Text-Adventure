4b420021cf98d691d78088d5fa43d95673222b31
Jack in the Boxx <kdijahsweets@gmail.com>
2020-03-13 21:10:27 -0500

    Finished! Fixed a lot of the breaks

--- Text Adventure/Program.cs
+++ Text Adventure/Program.cs
@@ -7,9 +7,10 @@ namespace Text_Adventure
 {
     public static class Program
     {
-        
+       
         static void Main(string[] args)
         {
+            
             Console.ForegroundColor = ConsoleColor.Blue;
 
             Console.Write(@"
@@ -22,6 +23,7 @@ ___________.__             ________                          __      _____      
 
        ");
 
+
        
             Console.ForegroundColor = ConsoleColor.DarkMagenta;
             DelayWrite("It is 8:00 am. You have a very important interview at " +
@@ -29,80 +31,66 @@ ___________.__             ________                          __      _____      
             DelayWrite("You haven't gotten much sleep all night due to high levels of excitement. " +
                 "Would you like to keep sleeping? yes/no:");
             string userSays = Console.ReadLine().ToLower();
-
             if(userSays == "yes")
             {
                 DelayWrite("I must warn you, going back to sleep now highly increases your" +
                     " chances of oversleeping and missing your interview." +
-                    "Are you sure you want to keep sleeping? yes/no:");
+                    "Are you sure you want to keep sleeping? Duh/no:");
+            }
+            Console.ForegroundColor = ConsoleColor.Green;
+            string keepSleep = Console.ReadLine().ToLower();
+            if (keepSleep == "duh")
+            {
+                DelayWrite("You Overslept!");
+                Console.Beep();               
             }            
-             if(userSays == "no")
+            else if (keepSleep == "no")
+            {
+                DelayWrite("Great! Let's get our day started then! I'll give you some time to wash yourself," +
+                    "eat breakfast, and grab a cup of coffee");
+            }
+            else if(userSays == "no")
             {
                 DelayWrite("I'm glad you're up! I'll give you some time to wash yourself, eat breakfast," +
                     " and grab a cup of coffee.");
-                Console.Write("Double tap ENTER to continue");
-            }
-            Console.ReadKey();              
-
-            string sleepResponse = Console.ReadLine().ToLower();
-
-            if(sleepResponse == "yes")
-            {
-                Console.WriteLine("You've overslept!");
-                Console.Beep();
-            }                                 
-            else if(sleepResponse == "no")
-            {
-                DelayWrite("Great! Let's get our day started then! I'll give you some time to wash yourself," +
-                    "eat breakfast, and grab a cup of coffee.");
             }
             
-            Console.ForegroundColor = ConsoleColor.Green;
-            DelayWrite("Now you that you have yourself all cleaned up, let's get your outfit ready." +
-                "I've already chosen a pantsuit for you to wear.");
-               
-            DelayWrite("Uh-Oh! Looks like your pants are missing! You only have 30 minutes left to get dressed!");
-            DelayWrite("How about this? I might know where your pants are. " +
-                "If you can answer this jokes I'll show you where they are. Ready? yes/no:");
+            Console.WriteLine();
+            DelayWrite("Alright! Now that you're all cleaned up, let's get your outfit ready. I've already chosen a pantsuit for you to wear.");           
+            Console.WriteLine();
+            DelayWrite("Uh-oh! It looks like your pants are missing. You've only got thirty minutes left to get dressed!");
+            Thread.Sleep(200);
+            Console.WriteLine();
+            Thread.Sleep(200);
+            DelayWrite("How about this? I might know where your pants are. If you can answer this joke correctly, I'll help you find them. Ready? yes/no");
+            Console.WriteLine();
             string readySays = Console.ReadLine().ToLower();
             if(readySays == "yes")
             {
-                DelayWrite("Alright! If I have just won the lottery for a million dollars, " +
-                    "and I decide to donate a quarter of it to charity, how much do I have left?");
+                DelayWrite("Awesome! Think very carefully about your answer, ok?");
+                Console.WriteLine();
+                DelayWrite("Ok. Here's the joke: If I have just won the lottery for a million dollars " +
+                    "and I decide to donate a quarter of it to charity, how much do I have left?");                   
             }
             else if(readySays == "no")
             {
                 DelayWrite("Whelp! You're not getting any help finding your pants! Good luck making it to your interview.");
             }
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.WriteLine();
             string answer = Console.ReadLine().ToLower();
             if(answer == "999,999.75")
             {
-                DelayWrite("Fine! You win. Here are your pants. Good luck on your interview!");
+                DelayWrite("You're very clever! You win. Here are your pants. Good luck on your interview.");
+
             }
-            else if(answer == "250,000")
+            else
             {
-                DelayWrite("I can see why you guessed that. But that's not right. Would you like to try again? Redo/No");
+                DelayWrite("You lose! Good luck finding your pants!");
             }
-            string redoResponse = Console.ReadLine().ToLower();
-            if (redoResponse == "Redo")
-            {
-                DelayWrite("Listen carefully. If I have just won the lottery for a million dollars, " +
-                    "and I decide to donate a quarter of it to charity, how much do I have left?");
-            }
-            else if (redoResponse == "no")
-            {
-                DelayWrite("Well then, good luck finding your pants.");
-            }
-            string newAnswer = Console.ReadLine();
-            if (newAnswer == "999,999.75")
-            {
-                DelayWrite("You win. Here your pants. Good luck on your interview.");
-            }
-            else if (newAnswer == "")
-            {
-                DelayWrite("You lose! Time is running out. Good luck finding your pants.");
-            }
+
         }
+          
 
         static void DelayWrite(string text, int charDelay = 50, bool DelayNewLine = true)
         {
